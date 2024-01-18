d2c2d180e0ee2c024934d40316657589e1d3ce35
imansai <im.saibari@gmail.com>
2024-01-18 15:34:10 -0800

    Add solutions 5, 6, 7, 8, 9, 10 and 11

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,14 @@
+-- Creates a function SafeDiv that divides the first by the second number 
+DROP FUNCTION IF EXISTS SafeDiv;
+DELIMITER $$
+CREATE FUNCTION SafeDiv (a INT, b INT)
+RETURNS FLOAT DETERMINISTIC
+BEGIN
+    DECLARE result FLOAT DEFAULT 0;
+
+    IF b != 0 THEN
+        SET result = a / b;
+    END IF;
+    RETURN result;
+END $$
+DELIMITER ;
